61d2b1f94af5e28674593fe968c33a9fe3b36b94
infomagnum159 <infomagnum159@gmail.com>
2022-07-21 16:26:02 +0600

    Created the main function that performs the function of adding, checking and clearing

---
+++ src/containers/Counter/Counter.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {BUTTONS} from '../../constants';
+import Button from '../../components/Button/Button';
+import ShowPassword from '../../components/ShowPassword/ShowPassword';
+import {useDispatch, useSelector} from "react-redux";
+import './Counter.css';
+
+
+const Counter = () => {
+    const password = useSelector(state => state.password);
+    const dispatch = useDispatch();
+
+    const deleteLastNumber = () => {
+        if(password.length > 4) {
+            dispatch({type: "CLEAR"});
+        }
+        dispatch( {type: 'DELETE'});
+    };
+    const addNumber = (event) => dispatch({type: 'ADD', password: event.target.value});
+    const verifyPassword = () => {
+        if (password.length > 4) return;
+        dispatch({type: 'VERIFY'});
+    };
+
+    return (
+        <>   <div className="password">
+            <ShowPassword value={password}/>
+        </div>
+            <div className="buttons">
+            {BUTTONS.map(value => (
+                <Button key={value} value={value} click={addNumber}></Button>
+            ))}
+            <Button click={deleteLastNumber} value='<'></Button>
+            <Button click={verifyPassword} value='E'></Button>
+            </div>
+        </>
+    );
+};
+
+export default Counter;
---
+++ src/store/reducer.js
@@ -0,0 +1,29 @@
+import {PASSWORD} from '../constants';
+
+const initialState = {
+    password: ''
+};
+
+const reducer = (state = initialState, action) => {
+    if (action.type === 'DELETE') {
+        let password = state.password.slice(0, -1);
+        return {...state, password};
+    }
+    if (action.type === 'ADD' && state.password.length < 4) {
+        return {...state, password: state.password + action.password};
+    }
+    if (action.type === 'VERIFY') {
+        if (state.password === PASSWORD) {
+
+            return {...state, password: 'Access Granted'};
+        } else {
+            return {...state, password: 'Access Denied'};
+        }
+    }
+    if (action.type === "CLEAR") {
+        return {...state, password: ''}
+    }
+    return state;
+};
+
+export default reducer;
